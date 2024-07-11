f924b0dd364fc77797c1fdbcedf6289c7ea899f9
joebusinessmoney <joejp103@gmail.com>
2024-07-11 19:27:10 +0100

    the canvas contents can now be saved as a png, the clear and paint function have been updated to account for PIL image

--- CustomFontMaker.py
+++ CustomFontMaker.py
@@ -1,4 +1,7 @@
 import tkinter as tk
+import PIL
+from PIL import Image, ImageDraw
+from tkinter import filedialog
 
 class FontPainter:
     brushSize = 7
@@ -17,7 +20,7 @@ class FontPainter:
 
         # create widgets used for saving, clearing and changing brush stize
 
-        self.button = tk.Button(root, text="Save")
+        self.button = tk.Button(root, text="Save", command=self.save)
         self.button.pack()
 
         self.button = tk.Button(root, text="Clear", command=self.clear)
@@ -34,6 +37,11 @@ class FontPainter:
 
         self.canvas.bind("<B1-Motion>", self.paint)
 
+        # create image to draw on
+
+        self.image = Image.new("RGB", (500, 500), "white")
+        self.draw = ImageDraw.Draw(self.image)
+
     # set size function
         
     def setSize(self):
@@ -46,12 +54,22 @@ class FontPainter:
         x1, y1 = (event.x - 1), (event.y - 1)
         x2, y2 = (event.x + 1), (event.y + 1)
         self.canvas.create_oval(x1, y1, x2, y2, fill="black", width=self.brushSize)
+        self.draw.circle([x1, y1, x2, y2], fill="black", outline="black", radius=self.brushSize)
 
     # clear function
 
     def clear(self):
         print("clear")
         self.canvas.delete("all")
+        self.image = Image.new("RGB", (500, 500), "white")
+        self.draw = ImageDraw.Draw(self.image)
+
+    # save function
+
+    def save(self):
+        filePath = filedialog.asksaveasfilename(defaultextension=".png")
+        if filePath:
+            self.image.save(filePath)
     
 # run application
 
