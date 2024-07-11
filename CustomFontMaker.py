a1defe5df5f23924e1298a45a976797c4bb33fa4
joebusinessmoney <joejp103@gmail.com>
2024-07-11 02:30:00 +0100

    setup frame

---
+++ CustomFontMaker.py
@@ -0,0 +1,19 @@
+from tkinter import *
+import os
+import pickle
+
+# window settings
+
+root = Tk()
+
+root.title("Custom Font Maker")
+root.geometry("500x500")
+root.resizable(False, False)
+
+# canvas frame
+
+frame1 = Frame(root, height=500, width=500)
+
+# canvas = Canvas(frame1, height=450, width=450, bg="white")
+
+root.mainloop()
